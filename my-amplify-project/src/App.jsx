dfc7341d54b9bc73bf78627498b48faac71d5631
scp-maeda <maedat@scala-partners.com>
2023-08-18 18:03:35 +0900

    スタイルの追加

--- my-amplify-project/src/App.jsx
+++ my-amplify-project/src/App.jsx
@@ -13,12 +13,12 @@ Amplify.configure(awsExports);
 import { createPost } from "./graphql/mutations";
 import { PostsByCreatedat } from './graphql/queries';
 
-import Header from './Header';
-
 export default function App() {
   const [posts, setPosts] = useState([]);
   const [postContent, setPostContent] = useState("");
   const [user, setUser] = useState(null);
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
 
   useEffect(() => {
     fetchPosts();
@@ -88,15 +88,17 @@ export default function App() {
                             <button onClick={signOut}>サインアウト</button>
                         </header>
                         <main>
-                            <div className="postForm">
-                                <textarea
-                                    value={postContent}
-                                    onChange={(e) => setPostContent(e.target.value)}
-                                    placeholder=""
-                                />
-                                <button onClick={addPost}>書き込む</button>
+                            <div className="rule">
+                              <h2>爆速開発掲示板</h2>
+                              <p>爆速で開発を進めるためのノウハウを共有する掲示板です。</p>
+                              <ul>
+                                <li>荒らし行為は禁止です。ルールを守って健全に利用しましょう。</li>
+                                <li>具体的な問題点や解決策を共有する際は、詳細を書き込んでください。</li>
+                                <li>過去の投稿は検索機能を使用して探すことができます。</li>
+                                <li>情報の正確さを第一に考え、信憑性の低い情報の共有は避けましょう。</li>
+                              </ul>
+                              <p>全てのユーザーが快適に利用できるようにご協力ください。</p>
                             </div>
-
                             <div className="postsList">
                                 {posts.map((post, index) => (
                                     <div key={post.id} className="post">
@@ -105,6 +107,25 @@ export default function App() {
                                     </div>
                                 ))}
                             </div>
+                            <div className="postForm">
+                              <div className="userInfo">
+                                <div className="inputGroup">
+                                  <label htmlFor="name">名前:</label>
+                                  <input type="text" id="name" placeholder="名無し" value={userName} onChange={(e) => setUserName(e.target.value)} />
+                                </div>
+                                <div className="inputGroup">
+                                  <label htmlFor="email">Eメール:</label>
+                                  <input type="email" id="email" placeholder="example@example.com" value={userEmail} onChange={(e) => setUserEmail(e.target.value)} />
+                                </div>
+                              </div>
+                              <textarea
+                                value={postContent}
+                                onChange={(e) => setPostContent(e.target.value)}
+                                placeholder="本文"
+                              />
+                              <button onClick={addPost}>書き込む</button>
+                            </div>
+
                         </main>
                     </div>
                 );
