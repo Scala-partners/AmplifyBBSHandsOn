ef9dc600c994d3d63d71c58b0e317293c31752db
scp-maeda <maedat@scala-partners.com>
2023-08-21 10:27:17 +0900

    書き込みに対するコメント追加とeslint導入

--- my-amplify-project/src/App.jsx
+++ my-amplify-project/src/App.jsx
@@ -19,44 +19,73 @@ export default function App() {
   const [user, setUser] = useState(null);
   const [userName, setUserName] = useState("");
   const [userEmail, setUserEmail] = useState("");
+  const [selectedPost, setSelectedPost] = useState(null);
+  const [commentContent, setCommentContent] = useState('');
+  const [showCommentPopup, setShowCommentPopup] = useState(false);
 
   useEffect(() => {
     fetchPosts();
   }, []);
 
-  async function fetchPosts() {
-    try {
-      const postData = await API.graphql(graphqlOperation(PostsByCreatedat, {
-        type: "POST",
-        sortDirection: 'DESC'  // 最新の投稿から順に取得
-      }));
-      setPosts(postData.data.PostsByCreatedat.items);
-    } catch (err) {
-      console.error("Error fetching posts: ", err);
-    }
+  const mockComments = {
+    "21e5ae94-c284-452c-9298-8517e359ad7f": [
+      { content: "モダンなフロントエンド技術について行くの大変だよ\nいつ勉強したらいいんだ", username: "名無し", createdAt: "2023-08-21 09:45" },
+      { content: "hoge", username: "fuga", createdAt: "2023-08-21 09:51" },
+      { content: "piyo", username: "foo", createdAt: "2023-08-21 10:20" }
+    ]
+};
+
+const openCommentPopup = post => {
+  setSelectedPost(post);
+  setShowCommentPopup(true);
+};
+
+const textWithBreaks = text => ({ __html: text.replace(/\n/g, '<br />') });
+
+const addCommentToPost = post => {
+  if (commentContent.trim()) {
+    mockComments[post.id] = mockComments[post.id] || [];
+    mockComments[post.id].push(commentContent);
+    setCommentContent('');
+    setShowCommentPopup(false);
   }
-  
-  function formatDate(dateString) {
+};
+
+const fetchPosts = async () => {
+  try {
+    const postData = await API.graphql(graphqlOperation(PostsByCreatedat, {
+      type: "POST",
+      sortDirection: 'DESC'  // 最新の投稿から順に取得
+    }));
+    console.log(postData.data.PostsByCreatedat.items);
+    setPosts(postData.data.PostsByCreatedat.items);
+  } catch (err) {
+    console.error("Error fetching posts: ", err);
+  }
+};
+
+const formatDate = dateString => {
   const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
   return new Date(dateString).toLocaleDateString(undefined, options);
-}
-  async function checkUser() {
-    try {
-      const currentUser = await Auth.currentAuthenticatedUser();
-      setUser(currentUser);
-    } catch (err) {
-      console.error(err);
-    }
-  }
+};
 
-  async function handleSignOut() {
-    try {
-      await Auth.signOut();
-      setUser(null);
-    } catch (err) {
-      console.error('Error signing out: ', err);
-    }
+const checkUser = async () => {
+  try {
+    const currentUser = await Auth.currentAuthenticatedUser();
+    setUser(currentUser);
+  } catch (err) {
+    console.error(err);
   }
+};
+
+const handleSignOut = async () => {
+  try {
+    await Auth.signOut();
+    setUser(null);
+  } catch (err) {
+    console.error('Error signing out: ', err);
+  }
+};
 
   return (
     <Authenticator>
@@ -101,9 +130,18 @@ export default function App() {
                             </div>
                             <div className="postsList">
                                 {posts.map((post, index) => (
-                                    <div key={post.id} className="post">
+                                    <div key={post.id} className="post" onClick={() => openCommentPopup(post)}>
                                         <h3>{index + 1}: {post.username} {formatDate(post.createdAt)}</h3>
                                         <p>{post.content}</p>
+                                        <div className="postComments">
+                                            {mockComments[post.id] && mockComments[post.id].map(comment => (
+                                                <div key={comment} className="singleComment">
+                                                    <span className="commentUser">{comment.username}</span>
+                                                    <span className="commentDate">{comment.createdAt}</span>
+                                                    <p dangerouslySetInnerHTML={textWithBreaks(comment.content)}></p>
+                                                </div>
+                                            ))}
+                                        </div>
                                     </div>
                                 ))}
                             </div>
@@ -125,6 +163,18 @@ export default function App() {
                               />
                               <button onClick={addPost}>書き込む</button>
                             </div>
+                            {showCommentPopup && (
+                                <div className="commentPopup">
+                                    <h4>コメントを追加 - {selectedPost.username}</h4>
+                                    <textarea
+                                        value={commentContent}
+                                        onChange={(e) => setCommentContent(e.target.value)}
+                                        placeholder="コメント内容"
+                                    />
+                                    <button onClick={() => addCommentToPost(selectedPost)}>追加</button>
+                                    <button onClick={() => setShowCommentPopup(false)}>閉じる</button>
+                                </div>
+                            )}
 
                         </main>
                     </div>
