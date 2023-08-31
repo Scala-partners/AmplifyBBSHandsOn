c4f0f4039001df61d635b0f840d8cc71111e8fdf
EC2 Default User <ec2-user@ip-172-31-43-234.ap-northeast-1.compute.internal>
2023-08-31 08:48:52 +0000

    環境の構築まで

---
+++ README.md
@@ -0,0 +1,3 @@
+# AmplifyBBSHandsOn
+本リポジトリの[Wiki](https://github.com/Scala-partners/AmplifyBBSHandsOn/wiki)を確認してください。
+
---
+++ my-amplify-project/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "myamplifyproject": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "myamplifyproject50f12a22": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
